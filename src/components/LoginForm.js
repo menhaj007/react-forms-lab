64c4a45b160dd4634ed2cf861f6f6b09ce62b2e7
menhaj <menhaj007@yahoo.com>
2021-05-03 13:47:49 -0400

    commit tree e263c49e2b2b1b6c6eea10c3b95a1e6da55ea970

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,62 @@
 import React from "react";
 
 class LoginForm extends React.Component {
-  constructor() {
+  constructor(props) {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: "",
+      submittedData: []
+    };
   }
+
+  handleInputChange = (e) => {
+    console.log(e.target.value)
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  submitHandler = (e) => {
+    e.preventDefault();
+    let formData = {username: this.state.username, password: this.state.password}
+    let arrayData = this.state.submittedData.concat(formData);
+    this.setState({
+      submittedData: arrayData
+    })
+  }
+  validation = (e) => {
+    if (e.target.value.length > 0) {
+      
+    }
+  }
+
 
   render() {
     return (
-      <form>
+      <form onSubmit={e => this.submitHandler(e)}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input 
+            id="username" 
+            name="username" 
+            type="text"
+            value={this.state.username}
+            onChange={e => this.handleInputChange(e)}
+            />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input 
+            id="password"
+            name="password" 
+            type="password" 
+            value={this.state.password}
+            onChange={e => this.handleInputChange(e)}
+            />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,38 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
+  constructor(props) {
     super();
 
-    this.state = {};
+    this.state = {
+      maxChars: props.maxChars,
+      counter: 0,
+      message: "",
+      messages: []
+    };
   }
+
+  handleMacChars = (chars) => {
+    // if (this.state.message.length )
+  }
+
+  handleMessage = (e) => {
+    this.setState({counter: this.state.counter+1})
+    if (this.state.counter < this.state.maxChars) {
+      this.setState({message: e.target.value})
+    }
+    // this.state.message.length < this.props.maxChars? this.setState({message: e.target.value}): this.state.message;
+  }
+
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.message} 
+        onChange={e =>this.handleMessage(e)}
+        />
+        <h1>Counter: {this.state.maxChars - this.state.counter}</h1>
       </div>
     );
   }
